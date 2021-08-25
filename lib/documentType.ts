5356752621f2a1d478626368e8d01515c07c6bee
Neha Bhanushali <Neha.Bhanushali@kibocommerce.com>
2021-08-25 15:57:50 +0530

    ICKY183 Added code changes for creating documentType successfully

--- lib/documentType.ts
+++ lib/documentType.ts
@@ -1,82 +1,13 @@
 var appsClient = require("mozu-node-sdk/clients/platform/application")();
 
-function log(result) {
-  console.log(result);
-}
+const documentTypeData = require("../data/documentType.json");
 
-function reportError(error) {
-  console.error(error.message, error);
-}
-
-var data = {
-  data: [
-    {
-      name: "banner",
-      namespace: "mozuadmin",
-      documentTypeFQN: "banner@mozuadmin",
-      metadata: {
-        isWebPage: false,
-      },
-      properties: [
-        {
-          name: "tag",
-          isRequired: false,
-          isMultiValued: true,
-          propertyType: {
-            name: "string",
-            namespace: "mozu",
-            propertyTypeFQN: "string@mozu",
-            installationPackage: "core@mozu",
-            version: "1.0",
-            dataType: "string",
-          },
-        },
-        {
-          name: "slot",
-          isRequired: false,
-          isMultiValued: false,
-          propertyType: {
-            name: "string",
-            namespace: "mozu",
-            propertyTypeFQN: "string@mozu",
-            installationPackage: "core@mozu",
-            version: "1.0",
-            dataType: "string",
-          },
-        },
-        {
-          name: "title",
-          isRequired: true,
-          isMultiValued: false,
-          propertyType: {
-            name: "string",
-            namespace: "mozu",
-            propertyTypeFQN: "string@mozu",
-            installationPackage: "core@mozu",
-            version: "1.0",
-            dataType: "string",
-          },
-        },
-        {
-          name: "subtitle",
-          isRequired: false,
-          isMultiValued: false,
-          propertyType: {
-            name: "string",
-            namespace: "mozu",
-            propertyTypeFQN: "string@mozu",
-            installationPackage: "core@mozu",
-            version: "1.0",
-            dataType: "string",
-          },
-        },
-      ],
-    },
-  ],
-};
-//for document type
 var documentType = require("mozu-node-sdk/clients/content/documentType")(
   appsClient
 );
-//documentType.getDocumentType().then(log, reportError);
-documentType.createDocumentType( data.data[0] ).then(log, reportError);
+
+//Calling function to create documentType
+documentType
+  .createDocumentType(documentTypeData.data[0])
+  .then((apiResult) => console.log(apiResult))
+  .catch((err) => console.log(err));
