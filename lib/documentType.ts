e33d2b857a48e14bde3d6bbc9eedb4c8f8635582
Neha Bhanushali <Neha.Bhanushali@kibocommerce.com>
2021-08-27 19:05:19 +0530

    Added code changes for creating documentType based on conditional flag

--- lib/documentType.ts
+++ lib/documentType.ts
@@ -1,13 +1,50 @@
-var appsClient = require("mozu-node-sdk/clients/platform/application")();
+var fs = require("fs");
+var path = require("path");
 
-const documentTypeData = require("../data/documentType.json");
+var { parse } = require("@jsonlines/core");
+
+var nconf = require("nconf");
+nconf.argv();
+
+const documentTypeData = nconf.get("import");
+
+var appsClient = require("mozu-node-sdk/clients/platform/application")();
 
 var documentType = require("mozu-node-sdk/clients/content/documentType")(
   appsClient
 );
 
-//Calling function to create documentType
-documentType
-  .createDocumentType(documentTypeData.data[0])
-  .then((apiResult) => console.log(apiResult))
-  .catch((err) => console.log(err));
+const dataFilePath = path.join(__dirname, "../");
+
+const source = fs.createReadStream(dataFilePath + documentTypeData);
+const parseStream = parse();
+const dataStream = source.pipe(parseStream);
+
+//processing data to create DocumentType
+
+(async function () {
+  for await (let documentTypeData of dataStream) {
+    await createDocumentType(documentTypeData);
+  }
+})();
+
+//function for creating documentType
+const createDocumentType = async (documentTypeData) => {
+  try {
+    const result = await documentType.createDocumentType(documentTypeData);
+    console.log("Successfully created document");
+  } catch (error) {
+    console.error("Error in creating Document", error);
+    if (error.originalError.statusCode === 409 && nconf.get("upsert")) {
+      try {
+        const updateDocumentTypeResult = await documentType.updateDocumentType(
+          { documentTypeName: documentTypeData.name },
+          documentTypeData
+        );
+        console.log("Updated DocumentType Successfully");
+      } catch (updateError) {
+        console.error("Error while updating document", updateError);
+      }
+    }
+  }
+};
