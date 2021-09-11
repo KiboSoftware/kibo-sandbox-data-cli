24dc6ffa95a8cc901b7df7390a82cc5cbb686cf4
Neha Bhanushali <Neha.Bhanushali@kibocommerce.com>
2021-09-11 21:23:23 +0530

    ICKY202-Added code for dataloads script for discounts (#10)
    * ICKY202-Added code for dataloads script for discounts
    
    ICKY202-Added code for dataloads script for discounts
    
    * ICKY199-Added code for data load scripts for categories scripts
    
    ICKY199-Added code for data load scripts for categories scripts

---
+++ lib/category.ts
@@ -0,0 +1,75 @@
+import path from "path";
+
+import { createJsonLFileStream, createAppsClientMozu } from "./utilites";
+
+import nconf from "nconf";
+
+nconf.argv();
+
+const categoriesDataPath = nconf.get("import") || nconf.get("clean");
+
+var appsClient = createAppsClientMozu();
+
+var categoryMethod =
+  require("mozu-node-sdk/clients/commerce/catalog/admin/category")(appsClient);
+
+const filePath = path.join(__dirname, "../");
+
+const dataFilePath = filePath + categoriesDataPath;
+
+let dataStream = createJsonLFileStream(dataFilePath);
+
+//function for creating discount
+const generateCategory = async (categoryData) => {
+  try {
+    await categoryMethod.addCategory(
+      { incrementSequence: true, useProvidedId: true },
+      { body: categoryData }
+    );
+    console.log("Successfully added categories");
+  } catch (error) {
+    console.error("Error in adding categories", error);
+    if (error.originalError.statusCode === 409 && nconf.get("upsert")) {
+      try {
+        await categoryMethod.updateCategory(
+          { categoryId: categoryData.categoryId, cascadeVisibility: false },
+          { body: categoryData }
+        );
+        console.log("Updated Categories Successfully");
+      } catch (updateError) {
+        console.error("Error while updating categories", updateError);
+      }
+    }
+  }
+};
+
+//below function will clean the data , delete category
+const deleteCategory = async (categoryData) => {
+  try {
+    await categoryMethod.deleteCategoryById({
+      categoryId: categoryData.categoryId,
+      cascadeDelete: true,
+      forceDelete: true,
+      reassignToParent: false,
+    });
+    console.log("Successfully deleted categories");
+  } catch (deleteError) {
+    console.error("Error while cleaning , deleting categories", deleteError);
+  }
+};
+
+//processing data to create/update cfategory
+if (nconf.get("import")) {
+  (async function () {
+    for await (let categoryDetail of dataStream) {
+      await generateCategory(categoryDetail);
+    }
+  })();
+} else if (nconf.get("clean")) {
+  //discount will be deleted
+  (async function () {
+    for await (let categoryDetail of dataStream) {
+      await deleteCategory(categoryDetail);
+    }
+  })();
+}
---
+++ lib/discount.ts
@@ -0,0 +1,70 @@
+import path from "path";
+
+import { createJsonLFileStream, createAppsClientMozu } from "./utilites";
+
+import nconf from "nconf";
+
+nconf.argv();
+
+const discountDataPath = nconf.get("import") || nconf.get("clean");
+
+var appsClient = createAppsClientMozu();
+
+var discountMethod =
+  require("mozu-node-sdk/clients/commerce/catalog/admin/discount")(appsClient);
+
+const filePath = path.join(__dirname, "../");
+
+const dataFilePath = filePath + discountDataPath;
+
+let dataStream = createJsonLFileStream(dataFilePath);
+
+//function for creating discount
+const generateDiscount = async (discountData) => {
+  try {
+    await discountMethod.createDiscount(discountData);
+    console.log("Successfully added discount");
+  } catch (error) {
+    console.error("Error in adding discount", error);
+    if (error.originalError.statusCode === 409 && nconf.get("upsert")) {
+      try {
+        console.log("disocount id ", discountData.thresholdMessage.discountId);
+        await discountMethod.updateDiscount(
+          { discountId: discountData.thresholdMessage.discountId },
+          { body: discountData }
+        );
+        console.log("Updated Discount Successfully");
+      } catch (updateError) {
+        console.error("Error while updating discount", updateError);
+      }
+    }
+  }
+};
+
+//below function will clean the data , delete discount
+const deleteDiscount = async (discountData) => {
+  try {
+    await discountMethod.deleteDiscount({
+      discountId: discountData.thresholdMessage.discountId,
+    });
+    console.log("Successfully deleted discount");
+  } catch (deleteError) {
+    console.error("Error while cleaning , deleting discount", deleteError);
+  }
+};
+
+//processing data to create/update discount
+if (nconf.get("import")) {
+  (async function () {
+    for await (let discountDetail of dataStream) {
+      await generateDiscount(discountDetail);
+    }
+  })();
+} else if (nconf.get("clean")) {
+  //discount will be deleted
+  (async function () {
+    for await (let discountDetail of dataStream) {
+      await deleteDiscount(discountDetail);
+    }
+  })();
+}
